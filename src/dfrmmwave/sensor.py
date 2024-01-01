3d78f0972957ab968b166b87dcb2a3297db6e726
stan svec <stan.x.svec@gmail.com>
2024-01-01 23:03:00 +0100

    simple presence handler

--- src/dfrmmwave/sensor.py
+++ src/dfrmmwave/sensor.py
@@ -1,4 +1,7 @@
+import re
 import time
+from typing import List, Tuple, Callable
+
 import serial
 
 
@@ -7,13 +10,38 @@ class Sensor:
     def __init__(self, serial_con):
         self.serial = serial_con
         self.stopped = False
+        self.handlers: List[Tuple[Callable[[str], bool], bool]] = [(self._presence_handler, False)]
+        self.observers: List[Callable[[bool], None]] = []
 
     def start(self):
         while not self.stopped:
             if self.serial.in_waiting > 0:
-                data = self.serial.readline()
-                print(data.decode('utf-8').rstrip())
+                msg_raw = self.serial.readline()
+                msg = msg_raw.decode('utf-8').rstrip()
+                self._handle_message(msg)
+                print(msg)
             time.sleep(1)
+
+    def _presence_handler(self, msg):
+        presence_match = re.match(r'^\$JYBSS,([01])', msg)
+        if presence_match:
+            return False
+
+        presence_value = presence_match.group(1)
+        for observer in self.observers:
+            observer(bool(presence_value))
+
+        return True
+
+    def _handle_message(self, msg):
+        if not self.handlers:
+            return
+
+        for handler, remove in self.handlers:
+            if handler(msg):
+                if remove:
+                    self.handlers.remove((handler, remove))
+                break
 
     def stop(self):
         self.stopped = True
